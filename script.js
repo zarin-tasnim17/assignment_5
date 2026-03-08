31cff90e707c3b7ef26739624d5f99ddb31e1570
Zarin Tasnim <zarin.tasnim.tech@gmail.com>
2026-03-09 03:13:55 +0600

    fetch API

--- script.js
+++ script.js
@@ -35,3 +35,53 @@ loginForm.addEventListener("submit", (e) => {
     loginError.classList.remove("hidden");
   }
 });
+async function fetchIssues() {
+  showLoader(true);
+  try {
+    const response = await fetch(`${API_BASE}/issues`);
+    const data = await response.json();
+
+
+    allIssuesData = Array.isArray(data) ? data : data.data || [];
+    renderIssues();
+  } catch (error) {
+    console.error("Error fetching issues:", error);
+    issuesGrid.innerHTML = `<p style="color:red">Failed to load issues.</p>`;
+  } finally {
+    showLoader(false);
+  }
+}
+
+
+async function searchIssues(query) {
+  if (!query.trim()) {
+    fetchIssues(); 
+    return;
+  }
+  showLoader(true);
+  try {
+    const response = await fetch(
+      `${API_BASE}/issues/search?q=${encodeURIComponent(query)}`,
+    );
+    const data = await response.json();
+    allIssuesData = Array.isArray(data) ? data : data.data || [];
+    renderIssues();
+  } catch (error) {
+    console.error("Error searching:", error);
+  } finally {
+    showLoader(false);
+  }
+}
+
+
+async function fetchSingleIssue(id) {
+  try {
+    const response = await fetch(`${API_BASE}/issue/${id}`);
+    const issue = await response.json();
+    return issue.data || issue;
+  } catch (error) {
+    console.error("Error fetching single issue", error);
+    return null;
+  }
+}
+
